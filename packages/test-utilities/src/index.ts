dc133edaabbf647b1b1c83156a4340fe69b38bdc
mainframev <vgenaev@gmail.com>
2025-07-08 13:19:30 +0200

    chore: suppress deprecation errors

--- packages/test-utilities/src/index.ts
+++ packages/test-utilities/src/index.ts
@@ -1,5 +1,6 @@
 export { createTestContainer } from './createTestContainer';
 export { getCSSRules } from './getCSSRules';
 export { safeCreate } from './safeCreate';
+// eslint-disable-next-line @typescript-eslint/no-deprecated
 export { safeMount } from './safeMount';
 export { create } from './reactTestRenderer';
